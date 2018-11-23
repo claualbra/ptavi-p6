204c59ed624075d07efd41935f144b978c08ceb0
Claudia Alvarez Bravo <claualbr@alpha40.aulas.gsyc.urjc.es>
2018-11-23 17:02:33 +0100

    segundo pep8

--- server.py
+++ server.py
@@ -30,7 +30,8 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                                  + b"SIP/2.0 180 Ringing\r\n\r\n"
                                  + b"SIP/2.0 200 OK\r\n\r\n")
             elif metodo == 'ACK':
-                # aEjecutar es un string con lo que se ha de ejecutar en la shell
+                # aEjecutar es un string
+                # con lo que se ha de ejecutar en la shell
                 aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + AUDIO
                 print("Vamos a ejecutar", aEjecutar)
                 os.system(aEjecutar)
@@ -41,6 +42,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             else:
                 self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
 
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     try:
@@ -49,7 +51,7 @@ if __name__ == "__main__":
         AUDIO = sys.argv[3]
     except (IndexError, ValueError):
         sys.exit("Usage: python3 server.py IP port audio_file")
-    serv = socketserver.UDPServer((IP,PORT), EchoHandler)
+    serv = socketserver.UDPServer((IP, PORT), EchoHandler)
     print("Listening...")
     try:
         serv.serve_forever()
