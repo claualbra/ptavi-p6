2beca2320d9e9f73c7d80ef288a0ba6715a60a8c
Claudia Alvarez Bravo <claualbr@alpha40.aulas.gsyc.urjc.es>
2018-11-23 16:59:20 +0100

    pep257

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
+"""Programa cliente que abre un socket a un servidor."""
 
 import socket
 import sys
@@ -18,7 +16,7 @@ try:
 except (IndexError, ValueError):
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
