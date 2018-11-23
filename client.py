2a907716d79a125a62206707fdb723c9d5184890
Claudia Alvarez Bravo <claualbr@alpha40.aulas.gsyc.urjc.es>
2018-11-23 16:55:10 +0100

    pep8 primera vez

--- client.py
+++ client.py
@@ -25,12 +25,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
 
     print("Enviando:", METODO + ' sip:' + ADRESS + ' SIP/2.0')
     my_socket.send(bytes(METODO + ' sip:' + ADRESS + ' SIP/2.0', 'utf-8')
-                    + b'\r\n\r\n')
+                   + b'\r\n\r\n')
     data = my_socket.recv(1024)
     recb = data.decode('utf-8').split()
     print('Recibido -- ', data.decode('utf-8'))
     if METODO == 'INVITE':
-        if recb[2] == 'Trying' and recb[5] == "Ringing" and recb[8]== "OK":
+        if recb[2] == 'Trying' and recb[5] == "Ringing" and recb[8] == "OK":
             my_socket.send(bytes('ACK' + ' sip:' + ADRESS + ' SIP/2.0', 'utf-8')
                             + b'\r\n\r\n')
     if METODO == 'BYE':
