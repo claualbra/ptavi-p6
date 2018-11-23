7c50a3dd62801706d69b94688ee2c1a314f2908b
Claudia Alvarez Bravo <claualbr@alpha40.aulas.gsyc.urjc.es>
2018-11-23 16:53:29 +0100

    servidor realizado

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import os
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -17,24 +18,28 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            linea = line.decode('utf-8')
-            print(line)
-            """
-            if linea != '':
-                print("El cliente nos manda ", linea)
-                (metodo, address, sip) = linea.split()
-                if metodo == 'INVITE':
-                    self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n"
-                                    + b"SIP/2.0 180 Ringing\r\n\r\n"
-                                    + b"SIP/2.0 200 OK\r\n\r\n")
-                elsif metodo != ('INVITE', 'ACK', 'BYE'):
-                    self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
-                else:
-                    self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
             # Si no hay más líneas salimos del bucle infinito
-            """
             if not line:
                 break
+            linea = line.decode('utf-8')
+            print("El cliente nos manda ", linea)
+            (metodo, address, sip) = linea.split()
+            if metodo == 'INVITE':
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n"
+                                + b"SIP/2.0 180 Ringing\r\n\r\n"
+                                + b"SIP/2.0 200 OK\r\n\r\n")
+            elif metodo == 'ACK':
+                # aEjecutar es un string con lo que se ha de ejecutar en la shell
+                aEjecutar = 'mp32rtp -i 127.0.0.1 -p 23032 < ' + AUDIO
+                print("Vamos a ejecutar", aEjecutar)
+                os.system(aEjecutar)
+            elif metodo == 'BYE':
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            elif metodo != ('INVITE', 'ACK', 'BYE'):
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+            else:
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     try:
